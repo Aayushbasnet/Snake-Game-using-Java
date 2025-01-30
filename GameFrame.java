f6c741fc74307ba65d80ec9a97ccf562c9c7e778
aayushbasnet07@gmail.com <aayushbasnet07@gmail.com>
2025-01-30 13:46:18 +1100

    game frame init

---
+++ GameFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;  // Import JFrame to create the game window
+public class GameFrame extends JFrame{
+    public GameFrame(){
+        // Set the title of the game window
+        this.setTitle("Snake Game");
+
+        // Set the default operation when closing the window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Prevent the user from resizing the window
+        this.setResizable(false);
+
+        // Set the size of the game window
+        this.setSize(600, 600);
+
+        // Center the window on the screen
+        this.setLocationRelativeTo(null);
+
+        // Make the window visible
+        this.setVisible(true);
+    }
+}
